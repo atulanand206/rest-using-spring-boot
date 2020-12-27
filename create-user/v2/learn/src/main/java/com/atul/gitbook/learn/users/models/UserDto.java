704bb3576873ef36001592b21f5fc345371899e4
Atul Anand <atulanand206@gmail.com>
2020-12-27 15:19:36 +0530

    :pencil: documentation for the user field validations.

--- create-user/v2/learn/src/main/java/com/atul/gitbook/learn/users/models/UserDto.java
+++ create-user/v2/learn/src/main/java/com/atul/gitbook/learn/users/models/UserDto.java
@@ -2,6 +2,14 @@ package com.atul.gitbook.learn.users.models;
 
 public class UserDto {
 
+    /**
+     * @param name  the name of the user.
+     * @param phone the phone number of the user.
+     * @param email the email of the user.
+     * @throws IllegalArgumentException in any of the following fails to be conformant:
+     *                                  1. Phone number : Must be 10 characters in length and all numeric.
+     *                                  2. Email : Must contain @ in the middle of the string.
+     */
     public UserDto(String name, String phone, String email) throws IllegalArgumentException {
         if (!phone.matches("[0-9]{10}")) {
             throw new IllegalArgumentException();
