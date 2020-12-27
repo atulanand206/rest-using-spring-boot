6f7aa8dcafa5eb4e292e6e8e2b08b095c2be97fb
Atul Anand <atulanand206@gmail.com>
2020-12-27 12:14:11 +0530

    Project setup.

---
+++ v1/learn/src/test/java/com/atul/gitbook/learn/LearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.gitbook.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
