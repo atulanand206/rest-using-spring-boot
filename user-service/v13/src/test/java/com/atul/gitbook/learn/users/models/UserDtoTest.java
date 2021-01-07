61132438b776fb12da00cfaac9137bdf9d73b452
Atul Anand <atulanand206@gmail.com>
2021-01-07 08:53:28 +0530

    :package: Refactored user service to have separate validations.

---
+++ user-service/v13/src/test/java/com/atul/gitbook/learn/users/models/UserDtoTest.java
@@ -0,0 +1,34 @@
+package com.atul.gitbook.learn.users.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class UserDtoTest {
+
+    @ParameterizedTest
+    @MethodSource("streamForCreateUserInvalidInput")
+    void testCreateUserInvalidInput(String name, String phone, String email) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new UserDto(name, phone, email));
+    }
+
+    private static Stream<Arguments> streamForCreateUserInvalidInput() {
+        return Stream.of(
+                Arguments.of("Julie", "78978297a4", "abc@de.com"),
+                Arguments.of("Julie", "789789728", "abc@de.com"),
+                Arguments.of("Julie", "7897839780", "abc@"),
+                Arguments.of(null, "7897897280", "abc@de.com"),
+                Arguments.of("Julie", null, "abc@de.com"),
+                Arguments.of("Julie", "7897897280", null)
+        );
+    }
+
+    @Test
+    void testCreateUserValidInput() {
+        Assertions.assertDoesNotThrow(() -> new UserDto("Julie", "7897897280", "abc@de.com"));
+    }
+}
